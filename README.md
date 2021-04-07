0e08d9539481744bea6cf0ef7fcf5ff41454db1c
Senchi3 <sensensenchi@gmail.com>
2021-04-07 13:06:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# parity.py
+ A small python command line game based on the "parity magic trick". Inspired by my Compsci class.
---
+++ parity.py
@@ -0,0 +1,23 @@
+import sys
+from termcolor import colored, cprint
+import time
+
+print("Printed immediately.")
+time.sleep(2.4)
+print("Printed after 2.4 seconds.")
+
+
+text = colored('Hello, World!', 'red', attrs=['reverse', 'blink'])
+print(text)
+cprint('Hello, World!', 'green', 'on_red')
+
+print_red_on_cyan = lambda x: cprint(x, 'red', 'on_cyan')
+print_red_on_cyan('Hello, World!')
+print_red_on_cyan('Hello, Universe!')
+
+for i in range(10):
+    cprint(i, 'magenta', end=' ')
+
+cprint("Attention!", 'red', attrs=['bold'], file=sys.stderr)
+
+input("Press Enter to continue...")
